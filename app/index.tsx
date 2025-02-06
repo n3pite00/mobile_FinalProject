582db1d69cb775f2e47072a88d7538282201503f
n3pite00 <n3pite00@students.oamk.fi>
2025-02-06 12:43:57 +0200

    Työalusta valmis.

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Location</Text>
+      <Button title="Add new location"></Button>
+
+    </View>
+  );
+}
