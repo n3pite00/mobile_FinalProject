1a33f54c29709daf6abe0dbe772fc88a93dc8ed9
n3pite00 <n3pite00@students.oamk.fi>
2025-02-17 21:13:27 +0200

    Korjattu Stack navigation.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <Stack>
+    <Stack.Screen name="index" options={{ title: 'Locations'}} />
+    <Stack.Screen name="AddLocation" options={{ title: 'Add location'}} />
+    <Stack.Screen name="MapView" options={{ title: 'Location on map'}} />
+  </Stack>
+  );
 }
